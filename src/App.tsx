3e46488cbe818225afcfb10cced7a1e93c20fbcb
siddhant1232 <siddhantgupta797400@gmail.com>
2025-03-14 22:00:09 +0530

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import CounterComp from "../components/counterComp";
 import ExpenseTracker from "../components/expenseTracker";
-import Weathercomp from "../components/Weathercomp"
+import Weathercomp from "../components/Weathercomp";
 
 const App = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
-      <div className="flex w-full max-w-4xl gap-6">
+      <div className="w-full max-w-4xl grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-        <div className="w-1/2 bg-white p-6 shadow-lg rounded-lg">
+
+        <div className="bg-white p-6 shadow-lg rounded-lg">
           <CounterComp />
         </div>
 
-        <div className="w-1/2 bg-white p-6 shadow-lg rounded-lg">
+
+        <div className="bg-white p-6 shadow-lg rounded-lg">
           <ExpenseTracker />
         </div>
-        <div className="w-1/2 bg-white p-6 shadow-lg rounded-lg">
-          <Weathercomp/>
+
+
+        <div className="bg-white p-6 shadow-lg rounded-lg">
+          <Weathercomp />
         </div>
 
       </div>
